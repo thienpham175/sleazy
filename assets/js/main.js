abd8fae33d5f64808d1f2ac9610820114bd971a7
thienpham175 <thienpdc@where.vn>
2025-10-23 10:20:41 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,5 @@
 window.__eventDataPromise.then(() => {
   JSON.parse(document.getElementById("script_event_data").textContent);
-  // Tiếp tục xử lý như cũ
-  // ladi viewport
-  // window.ladi_viewport = function (b) {
-  //   var a = document;
-  //   b = b ? b : "innerWidth";
-  //   var c = window[b];
-  //   var d = true;
-  //   if (
-  //     typeof window.ladi_is_desktop == "undefined" ||
-  //     window.ladi_is_desktop == undefined
-  //   ) {
-  //     window.ladi_is_desktop = !d;
-  //   }
-  //   var e = 960;
-  //   var f = 420;
-  //   var g = "";
-  //   if (!d) {
-  //     g = "width=" + e + ",user-scalable=yes,initial-scale=1.0";
-  //   } else {
-  //     var h = 1;
-  //     var i = f;
-  //     if (i != c) {
-  //       h = c / i;
-  //     }
-  //     g =
-  //       "width=" +
-  //       i +
-  //       ",user-scalable=no,initial-scale=" +
-  //       h +
-  //       ",minimum-scale=" +
-  //       h +
-  //       ",maximum-scale=" +
-  //       h;
-  //   }
-  //   var j = a.getElementById("viewport");
-  //   if (!j) {
-  //     j = a.createElement("meta");
-  //     j.id = "viewport";
-  //     j.name = "viewport";
-  //     a.head.appendChild(j);
-  //   }
-  //   j.setAttribute("content", g);
-  // };
-  // window.ladi_viewport();
-  // window.ladi_fbq_data = [];
-  // window.ladi_fbq = function () {
-  //   window.ladi_fbq_data.push(arguments);
-  // };
-  // window.ladi_ttq_data = [];
-  // window.ladi_ttq = function () {
-  //   window.ladi_ttq_data.push(arguments);
-  // };
 
   // music player
   var musicPlayer = document.getElementById("musicPlayer");
